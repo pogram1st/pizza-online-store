74fb5c8854d9678405301f51a0fc81350335fc4b
Konstantin <kolupaevkonstantin18@gmail.com>
2022-10-17 02:55:04 +0700

    added pizzas Card

--- react-pizza/src/App.js
+++ react-pizza/src/App.js
@@ -1,15 +1,22 @@
 import React from 'react';
 import { Route, Routes } from 'react-router-dom';
-
+import axios from 'axios';
 import Header from './components/Header';
 import Home from './pages/Home';
 import { Cart } from './pages/Cart';
 
 function App() {
+  const [pizzas, setPizzas] = React.useState([]);
   const [catigories, setCatigories] = React.useState(0);
   const clickOnCatigories = (obj) => {
     console.log(obj);
   };
+  React.useEffect(() => {
+    // fetch('http://localhost:3000/db.json')
+    // .then(data => (data.json()))
+    // .then(data => setPizzas(data.pizzas))
+    axios.get('http://localhost:3000/db.json').then((data) => setPizzas(data.data.pizzas));
+  }, []);
   return (
     <div className='wrapper'>
       <Header />
@@ -19,6 +26,7 @@ function App() {
             path='/'
             element={
               <Home
+                pizzas={pizzas}
                 clickOnCatigories={clickOnCatigories}
                 catigories={catigories}
                 setCatigories={setCatigories}
