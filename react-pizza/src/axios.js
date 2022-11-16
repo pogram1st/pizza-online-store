da89ab4b1b6847bdf796f23564263f85f87a9c72
Konstantin <kolupaevkonstantin18@gmail.com>
2022-11-16 17:28:01 +0700

    Переписал с Rdux на Redux Toolkit, пиццы теперь выгружаются с MongoDB, начал делать Вход и регистрацию

--- react-pizza/src/axios.js
+++ react-pizza/src/axios.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 const instance = axios.create({
-  baseURL: 'http://localhost:4444/',
+  baseURL: 'https://backend-pizza-o.herokuapp.com/',
 });
 instance.interceptors.request.use((config) => {
   config.headers.Authorization = window.localStorage.getItem('token');
